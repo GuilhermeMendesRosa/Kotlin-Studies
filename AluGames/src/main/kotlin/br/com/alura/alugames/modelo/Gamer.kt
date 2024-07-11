749f279506f53180cf3955bb3e0d83b6d232ed6a
GuilhermeMendesRosa <guilhermemr.jlle@gmail.com>
2024-07-11 20:48:22 -0300

    .

--- AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ AluGames/src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,3 +1,51 @@
 package br.com.alura.alugames.modelo
 
-data class Gamer()
+import kotlin.random.Random
+
+data class Gamer(var nome: String, var email: String) {
+    var dataNascimento: String? = null
+    var usuario: String? = null
+        set(value) {
+            field = value
+            if (idInterno.isNullOrBlank()) {
+                criarIdInterno()
+            }
+        }
+    var idInterno: String? = null
+        private set
+
+    init {
+        validarEmail()
+
+        if (nome.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome não pode ser vazio")
+        }
+    }
+
+    constructor(nome: String, email: String, dataNascimento: String, usuario: String) :
+            this(nome, email) {
+        this.dataNascimento = dataNascimento
+        this.usuario = usuario
+        criarIdInterno()
+    }
+
+    override fun toString(): String {
+        return "Gamer(" + nome + ", " + email + "," + idInterno + ")"
+    }
+
+    fun criarIdInterno() {
+        val numero = Random.nextInt(10000)
+        var tag = String.format("%04d", numero)
+        idInterno = "$usuario#$tag"
+    }
+
+    fun validarEmail(): String {
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if (regex.matches(email)) {
+            return email
+        }
+
+        throw IllegalArgumentException("Email inválido")
+    }
+
+}
--- AluGames/src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
+++ AluGames/src/main/kotlin/br/com/alura/alugames/principal/TesteGamer.kt
@@ -1,2 +1,19 @@
 package br.com.alura.alugames.principal
 
+import br.com.alura.alugames.modelo.Gamer
+
+fun main() {
+    val gamer1 = Gamer("Guilherme", "guilherme@email.com")
+    println(gamer1)
+
+    gamer1.let {
+        it.dataNascimento = "10/04/2003"
+        it.usuario = "gui"
+    }.also {
+        println(gamer1.idInterno)
+    }
+
+    val gamer2 = Gamer("Ana Paula", "ana@email.com", "11/02/2005", "aninha")
+    println(gamer2)
+    println(gamer1)
+}
